8df483d343afcd4e5a322ad41d54708d3b86a4ba
KARRI ADITYA <karri.aditya@tcs.com>
2017-11-03 17:22:27 +0530

    changed app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
 // Sample node.js web app for bTreePress How-To-Use-Docker-With-Continous-Integration-To-Build-A-DevOps-Automated-Workflow
- // For demonstration purposes only
+
  'use strict';
-  
+
+// Declaring function  
  var express = require('express'),
      app = express();
  
